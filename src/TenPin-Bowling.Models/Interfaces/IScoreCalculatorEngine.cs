30b654415d5dc76446dfd9407750ca8efd35cef0
Mahesh More <x-mahesh.more@coyote.com>
2022-04-27 14:28:56 +0530

    intial code changes

---
+++ src/TenPin-Bowling.Models/Interfaces/IScoreCalculatorEngine.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TenPin_Bowling.Domain.Interfaces
+{
+    using System.Threading.Tasks;
+    using TenPin_Bowling.Domain.Models;
+
+    public interface IScoreCalculatorEngine
+    {
+        Task<int> CalculateScoreAsync(Frame head);
+    }
+}
---
+++ src/TenPin-Bowling/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace TenPin_Bowling
+{
+    using System;
+    using System.Threading.Tasks;
+    using TenPin_Bowling.Engines;
+    using TenPin_Bowling.Common;
+    using TenPin_Bowling.Domain.Interfaces;
+
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            IHelper helper = new Helper();
+
+            var head = helper.CreateNode(null, 1, createStrike: true);
+            head = helper.CreateNode(head, 2);
+            head = helper.CreateNode(head, 2, createSpare: true);
+            head = helper.CreateNode(head, 2);
+            head = helper.CreateNode(head, 2);
+            head = helper.CreateNode(head, 2);
+            head = helper.CreateNode(head, 2);
+            head = helper.CreateNode(head, 2);
+            head = helper.CreateNode(head, 2);
+            head = helper.CreateNode(head, 1, createStrike: true);
+            head = helper.CreateNode(head, 1, createStrike: true);
+            head = helper.CreateNode(head, 1, createStrike: true);
+
+            var engine = new ScoreCalculatorEngine();
+
+            var score = await engine.CalculateScoreAsync(head).ConfigureAwait(false);
+
+            Console.WriteLine($"Total Score - {score}");
+            Console.ReadKey();
+        }
+
+
+    }
+}
